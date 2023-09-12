2a47631a6c6dee530d3d8083622925ea6e2ae7a5
wetfloo <yahahaha.1234567890@gmail.com>
2023-09-12 16:06:05 +0700

    Add native grep to telescope

--- nvim/lua/nvim/plugins/telescope.lua
+++ nvim/lua/nvim/plugins/telescope.lua
@@ -22,6 +22,11 @@ return {
                 },
             },
         })
+
+        -- Enable telescope fzf native, if installed
+
+        pcall(require('telescope').load_extension, 'fzf')
+
         local telescope = require('telescope.builtin')
 
         vim.keymap.set({ 'n' }, '<leader>ff', telescope.find_files, { desc = '[F]ind [F]iles' })
