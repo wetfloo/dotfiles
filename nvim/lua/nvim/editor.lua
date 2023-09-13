0c03842f63688daf03f0cc27c07e6fc2cdfa338d
wetfloo <yahahaha.1234567890@gmail.com>
2023-09-13 11:01:12 +0700

    Add resize keybinds to ideavimrc, another keybind revision

--- nvim/lua/nvim/editor.lua
+++ nvim/lua/nvim/editor.lua
@@ -44,8 +44,8 @@ vim.keymap.set({ 'n', 'x' }, '<A-d>', ':bdelete<CR>', { silent = true })
 
 vim.keymap.set('n', '<A-,>', ':bprev<CR>', { desc = 'Go to the previous buffer', silent = true })
 vim.keymap.set('n', '<A-.>', ':bnext<CR>', { desc = 'Go to the next buffer', silent = true })
-vim.keymap.set('n', '<A-x>', ':Bdelete<CR>', { desc = 'Delete the current buffer', silent = true })
-vim.keymap.set('n', '<A-X>', ':Bwipeout<CR>', { desc = 'Wipe out the current buffer', silent = true })
+vim.keymap.set('n', '<A-d>', ':Bdelete<CR>', { desc = 'Delete the current buffer', silent = true })
+vim.keymap.set('n', '<A-D>', ':Bwipeout<CR>', { desc = 'Wipe out the current buffer', silent = true })
 
 -- Open up a file explorer
 
