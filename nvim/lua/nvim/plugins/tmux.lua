39007eba7066dde51c66c82a8f0cb5b1402a5f85
wetfloo <yahahaha.1234567890@gmail.com>
2023-09-15 22:14:23 +0700

    Multiply key resize by 4, disable navigation cycling

--- nvim/lua/nvim/plugins/tmux.lua
+++ nvim/lua/nvim/plugins/tmux.lua
@@ -1,4 +1,18 @@
 return {
     'aserowy/tmux.nvim',
-    opts = {},
+    opts = {
+        navigation = {
+            cycle_navigation = false,
+            enable_default_keybindings = true,
+
+            -- prevents unzoom tmux when navigating beyond vim border
+
+            persist_zoom = false,
+        },
+        resize = {
+            enable_default_keybindings = true,
+            resize_step_x = 4,
+            resize_step_y = 4,
+        }
+    },
 }
