11e3e42d3ead2facb545eb1e214e41f19f2d6784
wetfloo <yahahaha.1234567890@gmail.com>
2023-09-29 22:18:15 +0700

    No tmux register sync, it's too confusing for me

--- nvim/lua/nvim/plugins/tmux.lua
+++ nvim/lua/nvim/plugins/tmux.lua
@@ -1,6 +1,16 @@
 return {
     'aserowy/tmux.nvim',
     opts = {
+        copy_sync = {
+            enable = false,
+            ignore_buffers = { empty = false },
+            redirect_to_clipboard = true,
+            register_offset = 0,
+            sync_clipboard = true,
+            sync_registers = true,
+            sync_deletes = true,
+            sync_unnamed = true,
+        },
         navigation = {
             cycle_navigation = false,
             enable_default_keybindings = true,
