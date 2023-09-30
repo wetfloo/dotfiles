095f921f6b90b3262ab9f2ca7d27f65e8c021aa5
wetfloo <yahahaha.1234567890@gmail.com>
2023-09-30 16:40:46 +0700

    WIP LSP setup

--- nvim/lua/nvim/plugins/lualine.lua
+++ nvim/lua/nvim/plugins/lualine.lua
@@ -7,5 +7,10 @@ return {
             component_separators = '|',
             section_separators = '',
         },
+        sections = {
+            lualine_c = {
+                'lsp_progress',
+            },
+        },
     },
 }
