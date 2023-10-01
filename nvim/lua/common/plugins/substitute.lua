021ac5bc73e3ac032f282fb118b4f279d830edd3
wetfloo <yahahaha.1234567890@gmail.com>
2023-10-01 16:08:06 +0700

    Non-conflicting keymap for substitute

--- nvim/lua/common/plugins/substitute.lua
+++ nvim/lua/common/plugins/substitute.lua
@@ -7,13 +7,13 @@ return {
         'BufReadPost',
         'BufNewFile',
     },
-    config = function(opts)
+    config = function()
         require('substitute').setup({})
         local exchange = require('substitute.exchange')
 
         vim.keymap.set("n", "cx", exchange.operator, { noremap = true })
         vim.keymap.set("n", "cxx", exchange.line, { noremap = true })
         vim.keymap.set("n", "cxc", exchange.cancel, { noremap = true })
-        vim.keymap.set("x", "X", exchange.visual, { noremap = true })
+        vim.keymap.set("x", "<leader>X", exchange.visual, { noremap = true })
     end,
 }
