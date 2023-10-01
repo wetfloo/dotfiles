5cb6222e153b7d05cb48b6b16b0c232afec49b5d
wetfloo <yahahaha.1234567890@gmail.com>
2023-10-01 16:10:22 +0700

    Add a utils module

---
+++ nvim/lua/utils.lua
@@ -0,0 +1,14 @@
+local M = {}
+
+--- Example
+---
+--- M.function_name = function(params)
+---     ...
+--- end
+---
+--- Then, in the target module:
+--- local module = require('utils')
+--- module.function_name(params)
+
+
+return M
