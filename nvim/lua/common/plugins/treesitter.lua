345cc254cb5a920bf28372ae0fc7b777ff3e9d6a
wetfloo <yahahaha.1234567890@gmail.com>
2023-10-01 20:37:49 +0700

    Proper folding w/ treesitter

--- nvim/lua/common/plugins/treesitter.lua
+++ nvim/lua/common/plugins/treesitter.lua
@@ -6,63 +6,78 @@ return {
     dependencies = {
         'nvim-treesitter/nvim-treesitter-textobjects',
     },
-    build = ":TSUpdate",
+    build = ':TSUpdate',
     config = function()
-        -- [[ Configure Treesitter ]]
-        -- See `:help nvim-treesitter`
+        -- Working folding, yay!
 
-        require('nvim-treesitter.configs').setup {
+        vim.opt.foldmethod = 'expr'
+        vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
 
-            -- Add languages to be installed here that you want installed for treesitter
+        -- Unfold everything upon entering a new buffer.
 
-            ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim' },
-            auto_install = true,
-            highlight = { enable = not vim.g.vscode },
-            indent = { enable = not vim.g.vscode, disable = { 'python' } },
-            incremental_selection = {
-                enable = true,
-                keymaps = {
-                    init_selection = '<c-space>',
-                    node_incremental = '<c-space>',
-                    scope_incremental = '<c-s>',
-                    node_decremental = '<M-space>',
-                },
-            },
-            textobjects = {
-                select = {
+        vim.api.nvim_create_autocmd(
+            'BufReadPost,FileReadPost',
+            {
+                command = 'normal zR',
+            }
+        )
+
+
+        require('nvim-treesitter.configs').setup(
+        ---@diagnostic disable-next-line: missing-fields
+            {
+
+                -- Add languages to be installed here that you want installed for treesitter
+
+                ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim' },
+                auto_install = true,
+                highlight = { enable = not vim.g.vscode },
+                indent = { enable = not vim.g.vscode, disable = { 'python' } },
+                incremental_selection = {
                     enable = true,
-                    lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
                     keymaps = {
-                        -- You can use the capture groups defined in textobjects.scm
-                        ['aa'] = '@parameter.outer',
-                        ['ia'] = '@parameter.inner',
-                        ['af'] = '@function.outer',
-                        ['if'] = '@function.inner',
-                        ['ac'] = '@class.outer',
-                        ['ic'] = '@class.inner',
+                        init_selection = '<c-space>',
+                        node_incremental = '<c-space>',
+                        scope_incremental = '<c-s>',
+                        node_decremental = '<M-space>',
                     },
                 },
-                move = {
-                    enable = true,
-                    set_jumps = true, -- whether to set jumps in the jumplist
-                    goto_next_start = {
-                        [']m'] = '@function.outer',
-                        [']]'] = '@class.outer',
+                textobjects = {
+                    select = {
+                        enable = true,
+                        lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+                        keymaps = {
+                            -- You can use the capture groups defined in textobjects.scm
+                            ['aa'] = '@parameter.outer',
+                            ['ia'] = '@parameter.inner',
+                            ['af'] = '@function.outer',
+                            ['if'] = '@function.inner',
+                            ['ac'] = '@class.outer',
+                            ['ic'] = '@class.inner',
+                        },
                     },
-                    goto_next_end = {
-                        [']M'] = '@function.outer',
-                        [']['] = '@class.outer',
-                    },
-                    goto_previous_start = {
-                        ['[m'] = '@function.outer',
-                        ['[['] = '@class.outer',
-                    },
-                    goto_previous_end = {
-                        ['[M'] = '@function.outer',
-                        ['[]'] = '@class.outer',
+                    move = {
+                        enable = true,
+                        set_jumps = true, -- whether to set jumps in the jumplist
+                        goto_next_start = {
+                            [']m'] = '@function.outer',
+                            [']]'] = '@class.outer',
+                        },
+                        goto_next_end = {
+                            [']M'] = '@function.outer',
+                            [']['] = '@class.outer',
+                        },
+                        goto_previous_start = {
+                            ['[m'] = '@function.outer',
+                            ['[['] = '@class.outer',
+                        },
+                        goto_previous_end = {
+                            ['[M'] = '@function.outer',
+                            ['[]'] = '@class.outer',
+                        },
                     },
                 },
-            },
-        }
+            }
+        )
     end,
 }
