897d7a7d377c4ac462f088850f384961adc0f2bd
wetfloo <yahahaha.1234567890@gmail.com>
2023-10-02 08:44:17 +0700

    Fidget for LSPs

---
+++ nvim/lua/nvim/plugins/fidget.lua
@@ -0,0 +1,6 @@
+return {
+    'j-hui/fidget.nvim',
+    tag = 'legacy', -- https://github.com/j-hui/fidget.nvim/tree/89b633f3bd0b7187f070236b348697de176860e2#installation
+    event = 'LspAttach',
+    opts = {},
+}
