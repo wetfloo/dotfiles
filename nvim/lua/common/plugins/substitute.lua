ced3bab2e120d912ab045c54e576958180c738bc
wetfloo <yahahaha.1234567890@gmail.com>
2023-10-02 12:50:23 +0700

    Quotation marks replacement

--- nvim/lua/common/plugins/substitute.lua
+++ nvim/lua/common/plugins/substitute.lua
@@ -1,7 +1,7 @@
 local leader_prefix = require('utils').leader_prefix
 
 return {
-    "gbprod/substitute.nvim",
+    'gbprod/substitute.nvim',
     keys = {
         { 'cx', mode = 'n' },
         { leader_prefix('X'), mode = 'x' },
@@ -10,9 +10,9 @@ return {
         require('substitute').setup({})
         local exchange = require('substitute.exchange')
 
-        vim.keymap.set("n", "cx", exchange.operator, { noremap = true })
-        vim.keymap.set("n", "cxx", exchange.line, { noremap = true })
-        vim.keymap.set("n", "cxc", exchange.cancel, { noremap = true })
-        vim.keymap.set("x", "<leader>X", exchange.visual, { noremap = true })
+        vim.keymap.set('n', 'cx', exchange.operator, { noremap = true })
+        vim.keymap.set('n', 'cxx', exchange.line, { noremap = true })
+        vim.keymap.set('n', 'cxc', exchange.cancel, { noremap = true })
+        vim.keymap.set('x', '<leader>X', exchange.visual, { noremap = true })
     end,
 }
