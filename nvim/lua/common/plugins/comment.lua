f97daada35daebb7c71447bb8c9acc86efd77fea
wetfloo <yahahaha.1234567890@gmail.com>
2023-10-02 12:48:50 +0700

    Better lazy loading for some plugins

--- nvim/lua/common/plugins/comment.lua
+++ nvim/lua/common/plugins/comment.lua
@@ -1,11 +1,7 @@
-return { 
+return {
     'numToStr/Comment.nvim',
-    keys = { 
-        'gc',
-    },
-    event = { 
-        'BufReadPost',
-        'BufNewFile',
+    keys = {
+        { 'gc', mode = { 'n', 'x' } },
     },
 
     -- This empty table is necessary, otherwise it doesn't run.
