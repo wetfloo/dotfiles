06a32e547cac1d30037334fce7471cf566d1b856
wetfloo <yahahaha.1234567890@gmail.com>
2023-10-05 11:27:14 +0700

    Increase which-key timeout

--- nvim/lua/nvim/plugins/which-key.lua
+++ nvim/lua/nvim/plugins/which-key.lua
@@ -3,7 +3,7 @@ return {
     event = 'VeryLazy',
     config = function()
         vim.o.timeout = true
-        vim.o.timeoutlen = 500
+        vim.o.timeoutlen = 1000
         require("which-key").setup(
             {
                 -- your configuration comes here
