d25540accffd8da4321f26825c8a57a53c8fd3f2
wetfloo <yahahaha.1234567890@gmail.com>
2023-10-06 08:47:04 +0700

    Nicer colors for nvim

--- nvim/lua/nvim/plugins/catppuccin.lua
+++ nvim/lua/nvim/plugins/catppuccin.lua
@@ -13,9 +13,15 @@ return {
                 show_end_of_buffer = false,
                 color_overrides = {
                     mocha = {
-                        base = '#14141f',
-                        mantle = '#101019',
-                        crust = '#0c0c13',
+                        base = '#14141f',     -- #1e1e2e
+                        mantle = '#101019',   -- #181825
+                        crust = '#0c0c13',    -- #11111b
+                        surface0 = '#22232f', -- #313244
+                        surface1 = '#2c2e3a', -- #45475a
+                        surface2 = '#3a3c4a', -- #585b70
+                        overlay0 = '#464958', -- #6c7086
+                        overlay1 = '#52566b', -- #7f849c
+                        overlay2 = '#5b6280', -- #9399b2
                     },
                 },
                 integrations = {
