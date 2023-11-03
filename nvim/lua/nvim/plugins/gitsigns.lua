0d3a2dc790a4bebb36f4e25bcef455cf33ed8004
wetfloo <yahahaha.1234567890@gmail.com>
2023-11-03 22:37:50 +0700

    descriptions for gitsigns

--- nvim/lua/nvim/plugins/gitsigns.lua
+++ nvim/lua/nvim/plugins/gitsigns.lua
@@ -11,30 +11,35 @@ return {
                         else
                             mode = ''
                         end
+
+                        if desc ~= nil then
+                            desc = desc .. ' (git)'
+                        end
+
                         vim.keymap.set(mode, '<leader>' .. keys, func, { buffer = bufnr, desc = desc })
                     end
 
                     local gs = package.loaded.gitsigns
 
                     -- Navigation
-                    map('kp', gs.prev_hunk)
-                    map('kn', gs.next_hunk)
+                    map('kp', gs.prev_hunk, 'Go to previous hunk')
+                    map('kn', gs.next_hunk, 'Go to next hunk')
 
                     -- Actions
-                    map('ks', gs.stage_hunk)
-                    map('kr', gs.reset_hunk)
+                    map('ks', gs.stage_hunk, 'Stage hunk', 'n')
+                    map('kr', gs.reset_hunk, 'Reset hunk', 'n')
                     -- It's important to keep these here to override all-mode stage and reset mappings above.
-                    map('ks', function() gs.stage_hunk { vim.fn.line('.'), vim.fn.line('v') } end, 'x')
-                    map('kr', function() gs.reset_hunk { vim.fn.line('.'), vim.fn.line('v') } end, 'x')
-                    map('kS', gs.stage_buffer)
-                    map('ku', gs.undo_stage_hunk)
-                    map('kR', gs.reset_buffer)
-                    map('kp', gs.preview_hunk)
-                    map('kb', function() gs.blame_line { full = true } end)
-                    map('kB', gs.toggle_current_line_blame)
-                    map('kc', gs.diffthis)
-                    map('kC', function() gs.diffthis('~') end)
-                    map('kd', gs.toggle_deleted)
+                    map('ks', function() gs.stage_hunk { vim.fn.line('.'), vim.fn.line('v') } end, 'Stage hunk', 'x')
+                    map('kr', function() gs.reset_hunk { vim.fn.line('.'), vim.fn.line('v') } end, 'Reset hunk', 'x')
+                    map('kS', gs.stage_buffer, 'Stage buffer')
+                    map('ku', gs.undo_stage_hunk, 'Undo staging hunk')
+                    map('kR', gs.reset_buffer, 'Reset buffer')
+                    map('kp', gs.preview_hunk, 'Preview hunk')
+                    map('kb', function() gs.blame_line { full = true } end, 'Blame line')
+                    map('kB', gs.toggle_current_line_blame, 'Toggle current line blame')
+                    map('kc', gs.diffthis, 'Diff this')
+                    map('kC', function() gs.diffthis('~') end, 'Diff this to one commit ago')
+                    map('kd', gs.toggle_deleted, 'Toggle deleted')
                 end,
             }
         )
