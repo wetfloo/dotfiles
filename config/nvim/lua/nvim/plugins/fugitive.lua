85004e6566c7892f7864e1995e88bf8b6378ad53
wetfloo <yahahaha.1234567890@gmail.com>
2024-03-05 23:28:56 +0700

    new git keymaps

--- config/nvim/lua/nvim/plugins/fugitive.lua
+++ config/nvim/lua/nvim/plugins/fugitive.lua
@@ -3,4 +3,14 @@ return {
     event = {
         'VeryLazy',
     },
+    config = function()
+        vim.keymap.set(
+            'n',
+            '<leader>kb',
+            function()
+                vim.cmd('G blame')
+            end,
+            { desc = 'Show git blame side window (git)' }
+        )
+    end,
 }
--- config/nvim/lua/nvim/plugins/gitsigns.lua
+++ config/nvim/lua/nvim/plugins/gitsigns.lua
@@ -27,15 +27,15 @@ return {
 
                     -- Actions
                     map('ks', gs.stage_hunk, 'Stage hunk', 'n')
-                    map('kr', gs.reset_hunk, 'Reset hunk', 'n')
+                    map('kXr', gs.reset_hunk, 'Reset hunk', 'n')
                     -- It's important to keep these here to override all-mode stage and reset mappings above.
                     map('ks', function() gs.stage_hunk { vim.fn.line('.'), vim.fn.line('v') } end, 'Stage hunk', 'x')
-                    map('kr', function() gs.reset_hunk { vim.fn.line('.'), vim.fn.line('v') } end, 'Reset hunk', 'x')
+                    map('kXr', function() gs.reset_hunk { vim.fn.line('.'), vim.fn.line('v') } end, 'Reset hunk', 'x')
                     map('kS', gs.stage_buffer, 'Stage buffer')
                     map('ku', gs.undo_stage_hunk, 'Undo staging hunk')
-                    map('kR', gs.reset_buffer, 'Reset buffer')
+                    map('kXb', gs.reset_buffer, 'Reset buffer')
                     map('kp', gs.preview_hunk, 'Preview hunk')
-                    map('kb', function() gs.blame_line { full = true } end, 'Blame line')
+                    map('kv', function() gs.blame_line({ full = true }) end, 'Blame line')
                     map('kB', gs.toggle_current_line_blame, 'Toggle current line blame')
                     map('kc', gs.diffthis, 'Diff this')
                     map('kC', function() gs.diffthis('~') end, 'Diff this to one commit ago')
