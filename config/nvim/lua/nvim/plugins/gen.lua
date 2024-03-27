2cd0816516573db66d1c5765b5b4b2619fb236bb
wetfloo <yahahaha.1234567890@gmail.com>
2024-03-27 10:48:59 +0700

    temp gen.nvim updates

--- config/nvim/lua/nvim/plugins/gen.lua
+++ config/nvim/lua/nvim/plugins/gen.lua
@@ -2,20 +2,28 @@ return {
     'David-Kunz/gen.nvim',
     config = function()
         local gen = require('gen')
+        gen.setup(
+            {
+                model = 'nous-hermes2:10.7b-solar-q4_K_M',
+                host = '10.0.0.96',
+                port = '11434',
+                display_mode = 'split',
+            }
+        )
 
-        gen.model = 'llama2-uncensored'
-
-        vim.keymap.set({ 'n', 'x' }, '<leader>?', function() vim.cmd('Gen') end, { desc = 'AI help', silent = true })
+        -- sending commands via vim.cmd is bugged
+        -- https://github.com/David-Kunz/gen.nvim/issues/16
+        vim.keymap.set({ 'n', 'x' }, '<leader>?', ':Gen<CR>', { desc = 'AI help', silent = true })
 
         gen.prompts['Elaborate_Text'] = {
             prompt = "Elaborate the following text:\n$text",
-            replace = true
+            replace = false,
         }
         gen.prompts['Fix_Code'] = {
             prompt =
             "Fix the following code. Only ouput the result in format ```$filetype\n...\n```:\n```$filetype\n$text\n```",
             replace = true,
-            extract = "```$filetype\n(.-)```"
+            extract = "```$filetype\n(.-)```",
         }
     end,
 }
