370592075457bcde16aa9ae74f9df894bf4b1c45
wetfloo <yahahaha.1234567890@gmail.com>
2024-03-31 21:25:44 +0700

    fidget updates

--- config/nvim/lua/nvim/plugins/fidget.lua
+++ config/nvim/lua/nvim/plugins/fidget.lua
@@ -1,6 +1,13 @@
 return {
     'j-hui/fidget.nvim',
-    tag = 'legacy', -- https://github.com/j-hui/fidget.nvim/tree/89b633f3bd0b7187f070236b348697de176860e2#installation
+    tag = 'v1.4.0',
     event = 'LspAttach',
-    opts = {},
+    opts = {
+        progress = {
+            suppress_on_insert = true,
+            display = {
+                render_limit = 3,
+            },
+        },
+    },
 }
