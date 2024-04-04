cf83957f23ba2fdd48b95e279a8cb45814010fd8
wetfloo <yahahaha.1234567890@gmail.com>
2024-04-04 11:27:31 +0700

    inline function

--- config/nvim/lua/nvim/plugins/lualine.lua
+++ config/nvim/lua/nvim/plugins/lualine.lua
@@ -1,67 +1,3 @@
-local function my_cfg()
-    local separator = '::'
-    local function buffer_not_empty()
-        return vim.fn.empty(vim.fn.expand('%:t')) ~= 1
-    end
-
-    local function diagnostic_symbol(sym)
-        return sym .. ' '
-    end
-
-    local symbols = require('nvim.prefs').diagnostic_signs
-
-    require('lualine').setup(
-        {
-            options = {
-                icons_enabled = true,
-                theme = 'auto',
-                component_separators = separator,
-                section_separators = ' ',
-            },
-            sections = {
-                -- Left side.
-                lualine_a = {
-                    'mode',
-                },
-                lualine_b = {},
-                lualine_c = {
-                    {
-                        'filename',
-                        cond = buffer_not_empty,
-                    },
-                    {
-                        'diagnostics',
-                        symbols = {
-                            error = diagnostic_symbol(symbols.Error),
-                            warn = diagnostic_symbol(symbols.Warn),
-                            info = diagnostic_symbol(symbols.Info),
-                            ok = diagnostic_symbol(symbols.Ok),
-                            hint = diagnostic_symbol(symbols.Hint),
-                        },
-                    },
-                },
-
-                -- Right side.
-                lualine_x = {
-                    'diff',
-                    'filetype',
-                    {
-                        'o:encoding',
-                        fmt = string.lower,
-                    },
-                    {
-                        'fileformat',
-                        icons_enabled = false,
-                    },
-                    'location',
-                },
-                lualine_y = {},
-                lualine_z = {},
-            },
-        }
-    )
-end
-
 return {
     'nvim-lualine/lualine.nvim',
     dependencies = {
@@ -71,6 +7,66 @@ return {
         -- Since we have a status line already, don't duplicate current mode display
         vim.o.showmode = false
 
-        my_cfg()
+        local separator = '::'
+        local function buffer_not_empty()
+            return vim.fn.empty(vim.fn.expand('%:t')) ~= 1
+        end
+
+        local function diagnostic_symbol(sym)
+            return sym .. ' '
+        end
+
+        local symbols = require('nvim.prefs').diagnostic_signs
+
+        require('lualine').setup(
+            {
+                options = {
+                    icons_enabled = true,
+                    theme = 'auto',
+                    component_separators = separator,
+                    section_separators = ' ',
+                },
+                sections = {
+                    -- Left side.
+                    lualine_a = {
+                        'mode',
+                    },
+                    lualine_b = {},
+                    lualine_c = {
+                        {
+                            'filename',
+                            cond = buffer_not_empty,
+                        },
+                        {
+                            'diagnostics',
+                            symbols = {
+                                error = diagnostic_symbol(symbols.Error),
+                                warn = diagnostic_symbol(symbols.Warn),
+                                info = diagnostic_symbol(symbols.Info),
+                                ok = diagnostic_symbol(symbols.Ok),
+                                hint = diagnostic_symbol(symbols.Hint),
+                            },
+                        },
+                    },
+
+                    -- Right side.
+                    lualine_x = {
+                        'diff',
+                        'filetype',
+                        {
+                            'o:encoding',
+                            fmt = string.lower,
+                        },
+                        {
+                            'fileformat',
+                            icons_enabled = false,
+                        },
+                        'location',
+                    },
+                    lualine_y = {},
+                    lualine_z = {},
+                },
+            }
+        )
     end
 }
