9f30d87b693c3f6b45d1325c91e0ae57d336d053
wetfloo <yahahaha.1234567890@gmail.com>
2024-04-04 11:46:19 +0700

    less saturated colors

--- config/nvim/lua/nvim/plugins/noirbuddy.lua
+++ config/nvim/lua/nvim/plugins/noirbuddy.lua
@@ -10,6 +10,15 @@ return {
             primary = '#dfb6fc',
             secondary = '#a7d2fa',
             background = '#000000',
+
+            diagnostic_error = '#ff6666',
+            diagnostic_warning = '#fff98f',
+            diagnostic_info = '#b3f9ff',
+            diagnostic_hint = '#b3d1ff',
+
+            diff_add = '#80ff91',
+            diff_change = '#a7d2fa',
+            diff_delete = '#ff6666',
         }
     },
 }
