c3428b6734b1b42b612b83accc22ad651cf29fef
wetfloo <yahahaha.1234567890@gmail.com>
2024-05-08 16:04:44 +0700

    toggle undotree

--- config/nvim/lua/nvim/plugins/undotree.lua
+++ config/nvim/lua/nvim/plugins/undotree.lua
@@ -1,4 +1,14 @@
 return {
     'mbbill/undotree',
     event = 'VeryLazy',
+    config = function()
+        vim.keymap.set(
+            'n',
+            '<leader>tu',
+            function()
+                vim.cmd('UndotreeToggle')
+            end,
+            { desc = 'Undotree toggle', }
+        )
+    end,
 }
