561751126205dcebe7594f626a337068085b1bea
wetfloo <yahahaha.1234567890@gmail.com>
2024-05-27 15:12:19 +0700

    luarocks is required, add it

--- config/nvim/lua/nvim/plugins/first_aid.lua
+++ config/nvim/lua/nvim/plugins/first_aid.lua
@@ -6,7 +6,22 @@ return {
     lazy = false,
     dependencies = {
         'nvim-lua/plenary.nvim',
-        'rest-nvim/rest.nvim',
+        {
+            "vhyrro/luarocks.nvim",
+            priority = 1000,
+            config = true,
+            opts = {
+                rocks = { "lua-curl", "nvim-nio", "mimetypes", "xml2lua" }
+            }
+        },
+        {
+            "rest-nvim/rest.nvim",
+            ft = "http",
+            dependencies = { "luarocks.nvim" },
+            config = function()
+                require("rest-nvim").setup()
+            end,
+        }
     },
     config = function()
         P = function(val)
