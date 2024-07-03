3cbdeeef872c24cb234286e37e14c5ee266d20ce
wetfloo <yahahaha.1234567890@gmail.com>
2024-07-03 22:17:44 +0700

    noirbuddy updates

--- config/nvim/lua/nvim/plugins/noirbuddy.lua
+++ config/nvim/lua/nvim/plugins/noirbuddy.lua
@@ -5,20 +5,29 @@ return {
     },
     lazy = false,
     priority = 1000,
-    opts = {
-        colors = {
-            primary = "#f5a9b8",
-            secondary = "#5bcefa",
-            background = "#000000",
+    config = function()
+        local colorbuddy = require("colorbuddy")
+        local noirbuddy = require("noirbuddy")
+        noirbuddy.setup({
+            colors = {
+                primary = "#f5a9b8",
+                secondary = "#5bcefa",
+                background = "#000000",
 
-            diagnostic_error = "#ff474a",
-            diagnostic_warning = "#ffe561",
-            diagnostic_info = "#61fffc",
-            diagnostic_hint = "#61fffc",
+                diagnostic_error = "#ff474a",
+                diagnostic_warning = "#ffe561",
+                diagnostic_info = "#61fffc",
+                diagnostic_hint = "#61fffc",
 
-            diff_add = "#1aff85",
-            diff_change = "#adf5ff",
-            diff_delete = "#ff6183",
-        },
-    },
+                diff_add = "#1aff85",
+                diff_change = "#adf5ff",
+                diff_delete = "#ff6183",
+            },
+        })
+
+        local Group = colorbuddy.Group
+        local colors = colorbuddy.colors
+
+        Group.new("@variable.member", colors.noir_2)
+    end,
 }
