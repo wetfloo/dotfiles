8aaaec03e9d02b153bac705dc522690980ebeea9
wetfloo <yahahaha.1234567890@gmail.com>
2024-11-21 15:09:34 +0700

    firenvim config

--- config/nvim/lua/common/plugins/firenvim.lua
+++ config/nvim/lua/common/plugins/firenvim.lua
@@ -1,27 +1,48 @@
 local local_settings = {
-    [".*google\\.\\w+/search.*"] = {
-        takeover = "never",
-        priority = 1,
+    ["https://code\\.o3.*"] = {
+        takeover = "always",
+        selector = 'div[class="view-lines"]',
+        content = "html",
     },
-    [".*figma.com.*"] = {
-        takeover = "never",
-        priority = 1,
+    ["https://play\\.kotlinlang\\.org.*"] = {
+        takeover = "always",
+        command = {
+            "set ft=kotlin",
+            "LspStop",
+        },
     },
 }
 
 return {
     "glacambre/firenvim",
+    enabled = true,
 
-    enabled = false,
     lazy = not vim.g.started_by_firenvim,
     build = function()
         vim.fn["firenvim#install"](0)
     end,
     config = function()
+        vim.api.nvim_create_autocmd({ "TextChanged", "TextChangedI" }, {
+            callback = function(e)
+                if vim.g.timer_started == true then
+                    return
+                end
+                vim.g.timer_started = true
+                vim.fn.timer_start(1000, function()
+                    vim.g.timer_started = false
+                    vim.cmd("silent write")
+                end)
+            end,
+        })
+
         vim.g.firenvim_config = {
             globalSettings = {
-                selector = "textarea",
+                takeover = "never",
+                priority = 0,
+                alt = "all",
+                cmdlineTimeout = 3000,
             },
+
             localSettings = vim.tbl_deep_extend(
                 "keep",
                 local_settings,
