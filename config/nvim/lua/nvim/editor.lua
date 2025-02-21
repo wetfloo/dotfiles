1b8dbd067e0afbe7bed1638794e075855835b91f
wetfloo <yahahaha.1234567890@gmail.com>
2025-02-21 11:49:53 +0700

    try out different tab options

--- config/nvim/lua/nvim/editor.lua
+++ config/nvim/lua/nvim/editor.lua
@@ -1,10 +1,10 @@
 local utils = require("utils")
 
 -- Avoid weird 8 space tabs
-vim.opt.tabstop = 4
+vim.opt.tabstop = 8
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
+vim.opt.expandtab = false
 
 vim.o.termguicolors = true
 
