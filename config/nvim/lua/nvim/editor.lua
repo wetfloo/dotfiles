234e800697ed1163699be6d0de9582d21c4e3872
wetfloo <yahahaha.1234567890@gmail.com>
2025-02-22 17:31:50 +0700

    noexpandtab

--- config/nvim/lua/nvim/editor.lua
+++ config/nvim/lua/nvim/editor.lua
@@ -4,7 +4,7 @@ local utils = require("utils")
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
+vim.opt.expandtab = false
 
 vim.o.termguicolors = true
 
