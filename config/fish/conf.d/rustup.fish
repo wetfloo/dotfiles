a943fdb7334355a27be18f2147933d47f4d5eff0
wetfloo <yahahaha.1234567890@gmail.com>
2025-03-01 07:32:26 +0500

    inline more fish stuff

--- config/fish/conf.d/rustup.fish
+++
@@ -1,5 +0,0 @@
-set env_file "$HOME/.cargo/env.fish"
-
-if test -e $env_file
-    source $env_file
-end
--- config/fish/config.fish
+++ config/fish/config.fish
@@ -46,6 +46,19 @@ if type -q cargo
     end
 end
 
+if test -e "$HOME/.cargo/env.fish"
+    source "$HOME/.cargo/env.fish"
+end
+
+function removepath
+    if set -l index (contains -i $argv[1] $PATH)
+        set --erase --universal fish_user_paths[$index]
+        echo "Updated PATH: $PATH"
+    else
+        echo "$argv[1] not found in PATH: $PATH"
+    end
+end
+
 alias shizukustart='adb shell sh /sdcard/Android/data/moe.shizuku.privileged.api/start.sh'
 
 alias rm='rm -i'
