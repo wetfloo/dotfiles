ae7fbb71be1883a9e86d56087f27354d122f1d8e
wetfloo <yahahaha.1234567890@gmail.com>
2025-03-28 13:12:12 +0500

    try out blink.cmp, remove snippet sources for now

--- config/nvim/lua/nvim/plugins/lsp.lua
+++ config/nvim/lua/nvim/plugins/lsp.lua
@@ -113,13 +113,57 @@ return {
             },
         },
         {
-            "hrsh7th/nvim-cmp",
-            dependencies = {
-                "L3MON4D3/LuaSnip",
-                "saadparwaiz1/cmp_luasnip",
-                "hrsh7th/cmp-nvim-lsp",
-                "rafamadriz/friendly-snippets",
+            "saghen/blink.cmp",
+            -- optional: provides snippets for the snippet source
+            dependencies = { "rafamadriz/friendly-snippets" },
+
+            -- use a release tag to download pre-built binaries
+            version = "1.*",
+            -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+            -- build = 'cargo build --release',
+            -- If you use nix, you can build from source using latest nightly rust with:
+            -- build = 'nix run .#build-plugin',
+
+            ---@module 'blink.cmp'
+            ---@type blink.cmp.Config
+            opts = {
+                -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+                -- 'super-tab' for mappings similar to vscode (tab to accept)
+                -- 'enter' for enter to accept
+                -- 'none' for no mappings
+                --
+                -- All presets have the following mappings:
+                -- C-space: Open menu or open docs if already open
+                -- C-n/C-p or Up/Down: Select next/previous item
+                -- C-e: Hide menu
+                -- C-k: Toggle signature help (if signature.enabled = true)
+                --
+                -- See :h blink-cmp-config-keymap for defining your own keymap
+                keymap = { preset = "enter" },
+
+                appearance = {
+                    -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+                    -- Adjusts spacing to ensure icons are aligned
+                    nerd_font_variant = "mono",
+                },
+
+                -- (Default) Only show the documentation popup when manually triggered
+                completion = { documentation = { auto_show = false } },
+
+                -- Default list of enabled providers defined so that you can extend it
+                -- elsewhere in your config, without redefining it, due to `opts_extend`
+                sources = {
+                    default = { "lsp", "path", "snippets", "buffer" },
+                },
+
+                -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+                -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+                -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+                --
+                -- See the fuzzy documentation for more information
+                fuzzy = { implementation = "prefer_rust_with_warning" },
             },
+            opts_extend = { "sources.default" },
         },
     },
     config = function()
@@ -169,7 +213,7 @@ return {
         local servers_auto = vim.tbl_deep_extend("keep", servers_mason_manual, servers_mason_auto)
 
         local capabilities = vim.lsp.protocol.make_client_capabilities()
-        capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
+        capabilities = require("blink.cmp").get_lsp_capabilities(capabilities)
 
         local function setup_opts(opts)
             return {
@@ -196,54 +240,5 @@ return {
         for name, opts in pairs(servers_manual) do
             lspconfig[name].setup(setup_opts(opts))
         end
-
-        local cmp = require("cmp")
-        local luasnip = require("luasnip")
-        require("luasnip.loaders.from_vscode").lazy_load()
-        luasnip.config.setup({})
-
-        cmp.setup(
-            ---@diagnostic disable-next-line: missing-fields
-            {
-                snippet = {
-                    expand = function(args)
-                        luasnip.lsp_expand(args.body)
-                    end,
-                },
-                mapping = cmp.mapping.preset.insert({
-                    ["<C-n>"] = cmp.mapping.select_next_item(),
-                    ["<C-p>"] = cmp.mapping.select_prev_item(),
-                    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-                    ["<C-f>"] = cmp.mapping.scroll_docs(4),
-                    ["<C-Space>"] = cmp.mapping.complete({}),
-                    ["<CR>"] = cmp.mapping.confirm({
-                        behavior = cmp.ConfirmBehavior.Replace,
-                        select = true,
-                    }),
-                    ["<Tab>"] = cmp.mapping(function(fallback)
-                        if cmp.visible() then
-                            cmp.select_next_item()
-                        elseif luasnip.expand_or_locally_jumpable() then
-                            luasnip.expand_or_jump()
-                        else
-                            fallback()
-                        end
-                    end, { "i", "s" }),
-                    ["<S-Tab>"] = cmp.mapping(function(fallback)
-                        if cmp.visible() then
-                            cmp.select_prev_item()
-                        elseif luasnip.locally_jumpable(-1) then
-                            luasnip.jump(-1)
-                        else
-                            fallback()
-                        end
-                    end, { "i", "s" }),
-                }),
-                sources = {
-                    { name = "nvim_lsp" },
-                    -- { name = "luasnip" },
-                },
-            }
-        )
     end,
 }
