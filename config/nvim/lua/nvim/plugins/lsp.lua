474350902b41ee9ad13737c3a57dea1dd2c6cd82
wetfloo <yahahaha.1234567890@gmail.com>
2025-04-16 15:18:32 +0500

    remove friendly-snippets dep

--- config/nvim/lua/nvim/plugins/lsp.lua
+++ config/nvim/lua/nvim/plugins/lsp.lua
@@ -114,8 +114,6 @@ return {
         },
         {
             "saghen/blink.cmp",
-            -- optional: provides snippets for the snippet source
-            dependencies = { "rafamadriz/friendly-snippets" },
 
             -- use a release tag to download pre-built binaries
             version = "1.*",
