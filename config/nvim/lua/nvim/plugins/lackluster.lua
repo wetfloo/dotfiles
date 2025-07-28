e74608edfa0dbbabc120087fe40dc010f379bd6e
wetfloo <yahahaha.1234567890@gmail.com>
2025-07-28 08:48:36 +0500

    Revert "feeling like lightmodemoding idk"
    This reverts commit 2cf0e73ac30976eeeb07b0d178326181973f3c8a.

--- config/nvim/lua/nvim/plugins/lackluster.lua
+++ config/nvim/lua/nvim/plugins/lackluster.lua
@@ -2,7 +2,7 @@ return {
 	"slugbyte/lackluster.nvim",
 	lazy = false,
 	priority = 1000,
-	enabled = false,
+	enabled = true,
 	init = function()
 		local lackluster = require("lackluster")
 		local color = lackluster.color -- blue, green, red, orange, black, lack, luster, gray1-9
