815975b29c14cd17655c2c4c7e603daeb6556451
wetfloo <yahahaha.1234567890@gmail.com>
2026-03-28 02:01:30 +0500

    add fish ssh port forward function

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -97,6 +97,10 @@ function nvi
 	nvim --cmd 'let g:lsp_status = 0' $argv
 end
 
+function sshpf -d "Port forward using ssh" -w "ssh" -a localport remoteport host
+	ssh -fNL $localport:127.0.0.1:$remoteport $host
+end
+
 # https://github.com/3rd/image.nvim/tree/4007cddc4cfc1b5ddd49744a38362e7b0432b3a0?tab=readme-ov-file#installing-imagemagick
 #if type -q magick and type -q brew and [ "$(which magick)" = "$(brew --prefix)/bin/magick)" ]
 #    set -x DYLD_LIBRARY_PATH "$(brew --prefix)/lib $DYLD_LIBRARY_PATH"
