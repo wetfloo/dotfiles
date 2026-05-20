a590e779f2af0d1a03160ca96c754a440777b464
wetfloo <yahahaha.1234567890@gmail.com>
2026-05-21 03:57:46 +0500

    add telescope lazy loading

--- config/nvim/lua/nvim/plugins/telescope.lua
+++ config/nvim/lua/nvim/plugins/telescope.lua
@@ -11,6 +11,10 @@ M.dependencies = {
 	require("nvim.plugins.lib.telescope-fzf-native"),
 }
 
+M.lazy = true
+
+M.cmd = { "Telescope" }
+
 M.opts = {
 	defaults = {
 		mappings = {
@@ -27,17 +31,10 @@ M.opts = {
 	},
 }
 
-function M:config(opts)
-	require("telescope").setup(opts)
-
-	-- Enable telescope fzf native, if installed
-	pcall(require("telescope").load_extension, "fzf")
-
-	local builtin = require("telescope.builtin")
-
+function M:init()
 	local function map_under_cursor(keys, fn, desc)
 		local uppered = (keys:gsub("(.)$", string.upper))
-		vim.keymap.set("n", "<leader>" .. keys, fn, { desc = desc })
+		vim.keymap.set("n", "<leader>" .. keys, function() fn() end, { desc = desc })
 
 		vim.keymap.set("n", "<leader>" .. uppered, "'<leader>" .. keys .. "' . \"\" . expand('<cword>')", {
 			desc = desc,
@@ -45,7 +42,7 @@ function M:config(opts)
 			expr = true,
 		})
 
-		vim.keymap.set("x", "<leader>" .. uppered, fn, { desc = desc })
+		vim.keymap.set("x", "<leader>" .. uppered, function() fn() end, { desc = desc })
 		-- TODO: Temp with yanks hack until getregion is in stable.
 		-- See: https://github.com/neovim/neovim/pull/27578
 		vim.keymap.set(
@@ -61,26 +58,48 @@ function M:config(opts)
 		)
 	end
 
-	map_under_cursor("fbb", builtin.buffers, "Find buffer")
+	map_under_cursor("fbb", function()
+		require("telescope.builtin").buffers()
+	end, "Find buffer")
 	map_under_cursor("fbg", function()
-		builtin.live_grep({ grep_open_files = true })
+		require("telescope.builtin").live_grep({ grep_open_files = true })
 	end, "Find with grep in open buffers")
 	map_under_cursor("fhf", function()
-		builtin.find_files({ hidden = true, no_ignore = true })
+		require("telescope.builtin").find_files({ hidden = true, no_ignore = true })
 	end, "Find hidden files")
 	map_under_cursor("fhg", function()
-		builtin.live_grep({ hidden = true, no_ignore = true })
+		require("telescope.builtin").live_grep({ hidden = true, no_ignore = true })
 	end, "Find with grep")
+	map_under_cursor("ff", function()
+		require("telescope.builtin").find_files()
+	end, "Find files")
+	map_under_cursor("fkf", function()
+		require("telescope.builtin").git_files()
+	end, "Find git files")
+	map_under_cursor("fkb", function()
+		require("telescope.builtin").git_branches()
+	end, "Find git branch")
+	map_under_cursor("fg", function()
+		require("telescope.builtin").live_grep()
+	end, "Find with grep")
+	map_under_cursor("fq", function()
+		require("telescope.builtin").help_tags()
+	end, "Find help")
+	map_under_cursor("fr", function()
+		require("telescope.builtin").oldfiles()
+	end, "Find recent")
+	map_under_cursor("fe", function()
+		require("telescope.builtin").diagnostics()
+	end, "Find diagnostics")
+	vim.keymap.set("n", "<leader>fl", function()
+		require("telescope.builtin").resume()
+	end, { desc = "Last picker" })
+end
 
-	map_under_cursor("ff", builtin.find_files, "Find files")
-	map_under_cursor("fkf", builtin.git_files, "Find git files")
-	map_under_cursor("fkb", builtin.git_branches, "Find git branch")
-	map_under_cursor("fg", builtin.live_grep, "Find with grep")
-	map_under_cursor("fq", builtin.help_tags, "Find help")
-	map_under_cursor("fr", builtin.oldfiles, "Find recent")
-	map_under_cursor("fe", builtin.diagnostics, "Find diagnostics")
-
-	vim.keymap.set("n", "<leader>fl", builtin.resume, { desc = "Last picker" })
+function M:opts(_)
+	-- Enable telescope fzf native, if installed
+	pcall(require("telescope").load_extension, "fzf")
+	return {}
 end
 
 return M
