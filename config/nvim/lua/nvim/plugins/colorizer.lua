2f253bffc490e1b3f963cdee7b4a6fe86ae19c4e
wetfloo <yahahaha.1234567890@gmail.com>
2026-05-21 00:06:23 +0500

    update colorizer conf

--- config/nvim/lua/nvim/plugins/colorizer.lua
+++ config/nvim/lua/nvim/plugins/colorizer.lua
@@ -2,9 +2,16 @@ local M = {
 	"catgoose/nvim-colorizer.lua",
 	priority = 1000,
 	lazy = false,
-	config = function()
-		require("colorizer").setup()
-	end,
+	opts = {
+		filetypes = {
+			["css"] = {
+				names = true,
+			},
+		},
+		user_default_options = {
+			names = false,
+		},
+	},
 }
 
 return M
