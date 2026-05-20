4cb2599f9b68018d3c634006e5a26eddb05c9dac
wetfloo <yahahaha.1234567890@gmail.com>
2026-05-21 04:41:58 +0500

    lazy load gitsigns, remove deprecated stuff

--- config/nvim/lua/nvim/plugins/gitsigns.lua
+++ config/nvim/lua/nvim/plugins/gitsigns.lua
@@ -4,53 +4,71 @@ local M = {
 	"lewis6991/gitsigns.nvim",
 }
 
-M.opts = {
-	on_attach = function(bufnr)
-		local function map(keys, func, desc, mode_override)
-			local mode
-			if mode_override ~= nil then
-				mode = mode_override
-			else
-				mode = "n"
-			end
+M.event = {
+	"User RealFileOpen",
+}
 
-			if desc ~= nil then
-				desc = desc .. " (git)"
-			end
+M.lazy = true
 
-			vim.keymap.set(mode, "<leader>" .. keys, func, { buffer = bufnr, desc = desc })
+function M:init()
+	local function map(keys, func, desc, mode_override)
+		local mode
+		if mode_override ~= nil then
+			mode = mode_override
+		else
+			mode = "n"
 		end
 
-		local gs = package.loaded.gitsigns
+		if desc ~= nil then
+			desc = desc .. " (git)"
+		end
 
-		-- Navigation
-		map("kp", gs.prev_hunk, "Go to previous hunk")
-		map("kn", gs.next_hunk, "Go to next hunk")
+		vim.keymap.set(mode, "<leader>" .. keys, func, { buffer = 0, desc = desc })
+	end
 
-		-- Actions
-		map("ks", gs.stage_hunk, "Stage hunk", "n")
-		map("kXh", gs.reset_hunk, "Reset hunk", "n")
-		-- It's important to keep these here to override all-mode stage and reset mappings above.
-		map("ks", function()
-			gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
-		end, "Stage hunk", "x")
-		map("kXh", function()
-			gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
-		end, "Reset hunk", "x")
-		map("kS", gs.stage_buffer, "Stage buffer")
-		map("ku", gs.undo_stage_hunk, "Undo staging hunk")
-		map("kXb", gs.reset_buffer, "Reset buffer")
-		map("kr", gs.preview_hunk, "Preview hunk")
-		map("kv", function()
-			gs.blame_line({ full = true })
-		end, "Blame line")
-		map("kB", gs.toggle_current_line_blame, "Toggle current line blame")
-		map("kc", gs.diffthis, "Diff this")
-		map("kC", function()
-			gs.diffthis("~")
-		end, "Diff this to one commit ago")
-		map("kd", gs.toggle_deleted, "Toggle deleted")
-	end,
-}
+	-- Navigation
+	map("kp", function()
+		require("gitsigns").nav_hunk("prev")
+	end, "Go to previous hunk")
+	map("kn", function()
+		require("gitsigns").nav_hunk("next")
+	end, "Go to next hunk")
+
+	-- Actions
+	map("ks", function()
+		require("gitsigns").stage_hunk()
+	end, "Stage hunk", "n")
+	map("kXh", function()
+		require("gitsigns").reset_hunk()
+	end, "Reset hunk", "n")
+	-- It's important to keep these here to override all-mode stage and reset mappings above.
+	map("ks", function()
+		require("gitsigns").stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+	end, "Stage hunk", "x")
+	map("kXh", function()
+		require("gitsigns").reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+	end, "Reset hunk", "x")
+	map("kS", function()
+		require("gitsigns").stage_buffer()
+	end, "Stage buffer")
+	map("kXb", function()
+		require("gitsigns").reset_buffer()
+	end, "Reset buffer")
+	map("kr", function()
+		require("gitsigns").preview_hunk()
+	end, "Preview hunk")
+	map("kv", function()
+		require("gitsigns").blame_line({ full = true })
+	end, "Blame line")
+	map("kB", function()
+		require("gitsigns").toggle_current_line_blame()
+	end, "Toggle current line blame")
+	map("kc", function()
+		require("gitsigns").diffthis()
+	end, "Diff this")
+	map("kC", function()
+		require("gitsigns").diffthis("~")
+	end, "Diff this to one commit ago")
+end
 
 return M
